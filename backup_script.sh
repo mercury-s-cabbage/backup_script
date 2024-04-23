013bd306237a257fbf821b2cfe8f8f531401d02e
lena <lenaphilip@mail.ru>
2024-04-23 09:31:29 +0000

    edit script

--- backup_script.sh
+++ backup_script.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
-file_to_backup="myfile.txt"
+#! /bin/bash
+file_to_backup=$1
+name=$2
 backup_file="backup_$(date +'%Y%m%d%H%M%S').txt"
-cp "$file_to_backup" "$backup_file"
-echo "Backup created: $backup_file
+if [ $name ]
+then
+    backup_file="$name"_"$(date +'%Y%m%d%H%M%S').txt"
+fi
+
+mkdir -p  "${name}_backup"
+
+cp "$file_to_backup" "${name}_backup/$backup_file"
+echo "Backup created at $(date +'%Y-%m-%d %H:%M:%S'): $backup_file" >>"${name}_backup/backup.log"
